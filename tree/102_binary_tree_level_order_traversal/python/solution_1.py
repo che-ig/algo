71b15c3dba7cf52f933849331b50992894c72c95
chernov.ig <mr.ivan.chernov@yandex.ru>
2026-06-20 13:13:37 +0300

    добавил задачу по деревья - 102

---
+++ tree/102_binary_tree_level_order_traversal/python/solution_1.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # time: O(n)
+    # mem: O(n)
+    def preOrder(
+        self, node: TreeNode, level: int, levels: list[list[int]]
+    ) -> list[list[int]]:
+        if node is None:
+            return levels
+        if level == len(levels):
+            levels.append([])
+        # делаем preOrder обход и добавляем вершину на текущий уровень в конец списка
+        levels[level].append(node.val)
+        levels = self.preOrder(node.left, level + 1, levels)
+        levels = self.preOrder(node.right, level + 1, levels)
+        return levels
+
+    def levelOrder(self, root: TreeNode | None) -> list[list[int]]:
+        return self.preOrder(root, 0, [])
